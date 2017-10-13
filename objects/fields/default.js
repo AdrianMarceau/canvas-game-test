a53e1eb3e7317a7e5005a6a18a5f30d92f935d07
AdrianMarceau <adrian.marceau@gmail.com>
2017-10-12 23:11:18 -0400

    Fixed the file path and animation steps for the default field.

--- objects/fields/default.js
+++ objects/fields/default.js
@@ -4,7 +4,7 @@ battleFieldIndex.indexField({
     fieldToken: 'default',
     fieldName: 'Default Field',
     fieldBackground: {
-        fileName: 'images/fields/default/background.png',
+        filePath: 'images/fields/default/background.png',
         basePosition: [-80, 0],
         frameSpeed: 1,
         frameLayout: 'vertical',
@@ -12,29 +12,29 @@ battleFieldIndex.indexField({
             // pan right
             startPosition: [+80, 0],
             endPosition: [-80, 0],
-            frameDuration: 2400
+            frameDuration: 1200
             }, {
             // pan left
             startPosition: [-80, 0],
             endPosition: [+80, 0],
-            frameDuration: 2400
+            frameDuration: 1200
             }]
         },
     fieldForeground: {
         filePath: 'images/fields/default/foreground.png',
-        basePosition: [-80, 0],
-        panelOffsetY: 8,
-        frameSpeed: 5,
+        basePosition: [-80, 2],
+        panelOffsetY: 0,
+        frameSpeed: 1,
         frameLayout: 'vertical',
         animationSteps: [{
             // pan down
-            startPosition: [0, -1],
-            endPosition: [0, +1],
+            startPosition: [0, -2],
+            endPosition: [0, +2],
             frameDuration: 480
             }, {
             // pan down
-            startPosition: [0, +1],
-            endPosition: [0, -1],
+            startPosition: [0, +2],
+            endPosition: [0, -2],
             frameDuration: 480
             },]
         }
